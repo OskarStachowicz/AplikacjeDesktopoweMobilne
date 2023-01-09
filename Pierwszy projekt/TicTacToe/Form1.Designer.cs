1374c1246ea65778ee7233783d1326350bb89872
Zabciol <104020296+Zabciol@users.noreply.github.com>
2023-01-09 14:21:10 +0100

    update

---
+++ Pierwszy projekt/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿
+namespace TicTacToe
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonPosition00 = new System.Windows.Forms.Button();
+            this.ButtonPosition01 = new System.Windows.Forms.Button();
+            this.ButtonPosition11 = new System.Windows.Forms.Button();
+            this.ButtonPosition21 = new System.Windows.Forms.Button();
+            this.ButtonPosition02 = new System.Windows.Forms.Button();
+            this.ButtonPosition12 = new System.Windows.Forms.Button();
+            this.ButtonPosition22 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.ButtonPosition20 = new System.Windows.Forms.Button();
+            this.ButtonPosition10 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.RestartButton = new System.Windows.Forms.Button();
+            this.StartButton = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ButtonPosition00
+            // 
+            this.ButtonPosition00.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition00.Location = new System.Drawing.Point(5, 5);
+            this.ButtonPosition00.Name = "ButtonPosition00";
+            this.ButtonPosition00.Size = new System.Drawing.Size(191, 142);
+            this.ButtonPosition00.TabIndex = 0;
+            this.ButtonPosition00.Text = " ";
+            this.ButtonPosition00.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPosition01
+            // 
+            this.ButtonPosition01.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition01.Location = new System.Drawing.Point(5, 155);
+            this.ButtonPosition01.Name = "ButtonPosition01";
+            this.ButtonPosition01.Size = new System.Drawing.Size(191, 142);
+            this.ButtonPosition01.TabIndex = 3;
+            this.ButtonPosition01.Text = " ";
+            this.ButtonPosition01.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPosition11
+            // 
+            this.ButtonPosition11.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition11.Location = new System.Drawing.Point(204, 155);
+            this.ButtonPosition11.Name = "ButtonPosition11";
+            this.ButtonPosition11.Size = new System.Drawing.Size(191, 142);
+            this.ButtonPosition11.TabIndex = 4;
+            this.ButtonPosition11.Text = " ";
+            this.ButtonPosition11.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPosition21
+            // 
+            this.ButtonPosition21.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition21.Location = new System.Drawing.Point(403, 155);
+            this.ButtonPosition21.Name = "ButtonPosition21";
+            this.ButtonPosition21.Size = new System.Drawing.Size(193, 142);
+            this.ButtonPosition21.TabIndex = 5;
+            this.ButtonPosition21.Text = " ";
+            this.ButtonPosition21.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPosition02
+            // 
+            this.ButtonPosition02.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition02.Location = new System.Drawing.Point(5, 305);
+            this.ButtonPosition02.Name = "ButtonPosition02";
+            this.ButtonPosition02.Size = new System.Drawing.Size(191, 143);
+            this.ButtonPosition02.TabIndex = 6;
+            this.ButtonPosition02.Text = " ";
+            this.ButtonPosition02.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPosition12
+            // 
+            this.ButtonPosition12.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition12.Location = new System.Drawing.Point(204, 305);
+            this.ButtonPosition12.Name = "ButtonPosition12";
+            this.ButtonPosition12.Size = new System.Drawing.Size(191, 143);
+            this.ButtonPosition12.TabIndex = 7;
+            this.ButtonPosition12.Text = " ";
+            this.ButtonPosition12.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPosition22
+            // 
+            this.ButtonPosition22.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition22.Location = new System.Drawing.Point(403, 305);
+            this.ButtonPosition22.Name = "ButtonPosition22";
+            this.ButtonPosition22.Size = new System.Drawing.Size(193, 143);
+            this.ButtonPosition22.TabIndex = 8;
+            this.ButtonPosition22.Text = " ";
+            this.ButtonPosition22.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Outset;
+            this.tableLayoutPanel1.ColumnCount = 3;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33334F));
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition22, 2, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition00, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition12, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition02, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition21, 2, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition01, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition20, 2, 0);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition11, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ButtonPosition10, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(53, 121);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(601, 453);
+            this.tableLayoutPanel1.TabIndex = 4;
+            // 
+            // ButtonPosition20
+            // 
+            this.ButtonPosition20.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition20.Location = new System.Drawing.Point(403, 5);
+            this.ButtonPosition20.Name = "ButtonPosition20";
+            this.ButtonPosition20.Size = new System.Drawing.Size(193, 142);
+            this.ButtonPosition20.TabIndex = 9;
+            this.ButtonPosition20.Text = " ";
+            this.ButtonPosition20.UseVisualStyleBackColor = true;
+            // 
+            // ButtonPosition10
+            // 
+            this.ButtonPosition10.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonPosition10.Location = new System.Drawing.Point(204, 5);
+            this.ButtonPosition10.Name = "ButtonPosition10";
+            this.ButtonPosition10.Size = new System.Drawing.Size(191, 142);
+            this.ButtonPosition10.TabIndex = 10;
+            this.ButtonPosition10.Text = " ";
+            this.ButtonPosition10.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.RestartButton);
+            this.panel1.Controls.Add(this.StartButton);
+            this.panel1.Location = new System.Drawing.Point(56, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(595, 100);
+            this.panel1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Kto zaczyna grę ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "kółko ",
+            "krzyżyk"});
+            this.comboBox1.Location = new System.Drawing.Point(25, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // RestartButton
+            // 
+            this.RestartButton.Location = new System.Drawing.Point(299, 40);
+            this.RestartButton.Name = "RestartButton";
+            this.RestartButton.Size = new System.Drawing.Size(75, 23);
+            this.RestartButton.TabIndex = 1;
+            this.RestartButton.Text = "restart";
+            this.RestartButton.UseVisualStyleBackColor = true;
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(200, 40);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(755, 586);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = " ";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonPosition00;
+        private System.Windows.Forms.Button ButtonPosition01;
+        private System.Windows.Forms.Button ButtonPosition11;
+        private System.Windows.Forms.Button ButtonPosition21;
+        private System.Windows.Forms.Button ButtonPosition02;
+        private System.Windows.Forms.Button ButtonPosition12;
+        private System.Windows.Forms.Button ButtonPosition22;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button ButtonPosition20;
+        private System.Windows.Forms.Button ButtonPosition10;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button RestartButton;
+        private System.Windows.Forms.Button StartButton;
+    }
+}
+
---
+++ Pierwszy projekt/simpleCalculator/mainForm.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalculatorApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdd_Click_TMP(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            if (Validate(textBoxFirsNumber.Text, textBoxSecondNumber.Text,
+                out firstNumber, out secondNumber))
+            {
+                Calculate('+', firstNumber, secondNumber);
+            }
+            else
+            {
+                ShowErrorMessage();
+            }
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            if (int.TryParse(textBoxFirsNumber.Text, out firstNumber)
+                && int.TryParse(textBoxSecondNumber.Text, out secondNumber))
+            {
+                int result = firstNumber + secondNumber;
+                labelResult.Text = "Wynik operacji dodawania: " + result;
+            }
+            else
+            {
+                labelResult.Text = "Podano nieprawidłowe dane";
+            }
+        }
+
+        private void buttonSub_Click(object sender, EventArgs e)
+        {
+            int firstNumber;
+            int secondNumber;
+            if (int.TryParse(textBoxFirsNumber.Text, out firstNumber)
+                && int.TryParse(textBoxSecondNumber.Text, out secondNumber))
+            {
+                int result = firstNumber - secondNumber;
+                labelResult.Text = "Wynik operacji odejmowania: " + result;
+            }
+            else
+            {
+                labelResult.Text = "Podano nieprawidłowe dane";
+            }
+        }
+
+        private void ShowErrorMessage()
+        {
+            labelResult.Text = "Podano nieprawidłowe dane";
+        }
+
+        private bool Validate(string firstStrNumber, string secondStrNumber,
+                              out int firstNumber, out int secondNumber)
+        {
+            //firstNumber = 0;
+            secondNumber = 0;
+            return int.TryParse(firstStrNumber, out firstNumber)
+                && int.TryParse(secondStrNumber, out secondNumber);
+        }
+
+        private void Calculate(char operatorToDo, int firstNumber, int secondNumber)
+        {
+            int result;
+            switch (operatorToDo)
+            {
+                case '+':
+                    result = firstNumber + secondNumber;
+                    labelResult.Text = "Wynik operacji dodawania: " + result;
+                    break;
+                case '-':
+                    result = firstNumber - secondNumber;
+                    labelResult.Text = "Wynik operacji odejmowania: " + result;
+                    break;
+                case '*':
+                    result = firstNumber * secondNumber;
+                    labelResult.Text = "Wynik operacji mnożenia: " + result;
+                    break;
+                case '/':
+                    result = firstNumber / secondNumber;
+                    labelResult.Text = "Wynik operacji dzielenia: " + result;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
